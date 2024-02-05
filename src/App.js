3b1232faddc59b10c68f510f4d1ac9024ce88f5d
devtgadani <devgadani2552@gmail.com>
2024-02-05 19:59:28 +0530

    fixing error

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import MainContent from './pages/homeScreen/MainContent';
 
 import Layout from './pages/leftNavigation/Layout';
-import NavigatorH from './pages/navigationHeader/NavigationHeader';
+import NavigatorHeader from './pages/navigationHeader/NavigationHeader';
 
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path='/' element={<NavigatorH />}>
+      <Route path='/' element={<NavigatorHeader />}>
         <Route path='' element={(<MainContent />)} />
 
         <Route path='docs' element={<Layout />}>
