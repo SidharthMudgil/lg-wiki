6d554f92d3561a83ecb3746ca11dde8cce5db913
devtgadani <devgadani2552@gmail.com>
2024-02-05 20:22:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import './App.css';
+import MainContent from './pages/homeScreen/MainContent';
+
+import Layout from './pages/leftNavigation/Layout';
+import NavigatorHeader from './pages/navigationHeader/NavigationHeader';
 
 
 function App() {
