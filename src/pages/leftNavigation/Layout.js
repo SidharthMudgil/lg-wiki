a478633bacff544c0ea8c66db82e04eea6041716
Arin Kulkarni <124277065+akoolarni@users.noreply.github.com>
2024-02-16 10:56:42 +0530

    added docs (#6)
    Co-authored-by: Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>

--- src/pages/leftNavigation/Layout.js
+++ src/pages/leftNavigation/Layout.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navigaton from "./Navigaton";
 
+import Doc from "../documentation/Doc";
+
+
 export default function Layout() {
   return (
     <>
       <div className="main h-screen">
         <Navigaton />
+        <Doc />
         <Outlet />
       </div>
     </>
